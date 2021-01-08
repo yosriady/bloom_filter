faba8ca44c4c7677c97dea8975b94088cfe6c0f3
Niklas Johansson <raphexion@gmail.com>
2021-01-08 09:09:41 +0100

    mix: Bump elixir version to 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BloomFilter.Mixfile do
     [
       app: :bloom_filter,
       version: "1.0.1",
-      elixir: "~> 1.2",
+      elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
