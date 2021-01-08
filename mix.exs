7c712b6d202ffa31ae7bf2cbe7b4e40420f5543f
Niklas Johansson <raphexion@gmail.com>
2021-01-08 08:35:37 +0100

    mix: Update dependencies

--- mix.exs
+++ mix.exs
@@ -2,15 +2,17 @@ defmodule BloomFilter.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bloom_filter,
-     version: "1.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description,
-     package: package,
-     deps: deps,
-     test_coverage: [tool: ExCoveralls]]
+    [
+      app: :bloom_filter,
+      version: "1.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description,
+      package: package,
+      deps: deps,
+      test_coverage: [tool: ExCoveralls]
+    ]
   end
 
   defp description do
@@ -22,12 +24,14 @@ defmodule BloomFilter.Mixfile do
 
   defp package do
     [
-     files: ["lib", "mix.exs", "README.md"],
-     maintainers: ["Yos Riady"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/yosriady/bloom_filter",
-              "Docs" => "http://hexdocs.pm/bloom_filter/"}
-     ]
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Yos Riady"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/yosriady/bloom_filter",
+        "Docs" => "http://hexdocs.pm/bloom_filter/"
+      }
+    ]
   end
 
   # Configuration for the OTP application
@@ -47,11 +51,13 @@ defmodule BloomFilter.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:math, "~> 0.2.0"},
-     {:fnv, "~> 0.3.2 "},
-     {:ex_doc, "~> 0.11", only: :dev},
-     {:earmark, "~> 0.1", only: :dev},
-     {:dialyxir, "~> 0.3", only: :dev},
-     {:excoveralls, "~> 0.5", only: :test}]
+    [
+      {:math, "~> 0.6"},
+      {:fnv, "~> 0.3.2 "},
+      {:ex_doc, "~> 0.23", only: :dev},
+      {:earmark, "~> 1.4", only: :dev},
+      {:dialyxir, "~> 1.0", only: :dev},
+      {:excoveralls, "~> 0.13.4", only: :test}
+    ]
   end
 end
