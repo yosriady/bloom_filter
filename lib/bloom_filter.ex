1335964f09f578874d80f49849759a331fb085c7
Niklas Johansson <raphexion@gmail.com>
2021-01-08 09:11:59 +0100

    Format files

--- lib/bloom_filter.ex
+++ lib/bloom_filter.ex
@@ -17,24 +17,30 @@ defmodule BloomFilter do
   @type bit :: 0 | 1
   @type hash_func :: (any -> pos_integer)
   @type t :: %BloomFilter{
-    bits: [bit, ...],
-    num_bits: pos_integer,
-    capacity: pos_integer,
-    error_rate: float,
-    hash_functions: [hash_func, ...]
-  }
+          bits: [bit, ...],
+          num_bits: pos_integer,
+          capacity: pos_integer,
+          error_rate: float,
+          hash_functions: [hash_func, ...]
+        }
 
   @doc """
     Creates a new bloom filter, given an estimated number of elements and a desired error rate (0.0..1).
   """
   @spec new(pos_integer, float) :: t
   def new(capacity, error_rate)
-  when error_rate > 0 and error_rate < 1 do
+      when error_rate > 0 and error_rate < 1 do
     {m, k, _} = optimize(capacity, error_rate)
-    bits = (for _ <- 1..m, do: 0)
+    bits = for _ <- 1..m, do: 0
     hash_functions = make_hashes(m, k)
-    %BloomFilter{ bits: bits, num_bits: m, capacity: capacity,
-                  error_rate: error_rate, hash_functions: hash_functions}
+
+    %BloomFilter{
+      bits: bits,
+      num_bits: m,
+      capacity: capacity,
+      error_rate: error_rate,
+      hash_functions: hash_functions
+    }
   end
 
   @doc """
@@ -43,9 +49,10 @@ defmodule BloomFilter do
   @spec has?(t, any) :: boolean
   def has?(%BloomFilter{bits: bits, hash_functions: hash_functions}, item) do
     item_bits = hash(hash_functions, item)
+
     item_bits
-    |> Enum.map(fn(x) -> Enum.at(bits, x) end)
-    |> Enum.all?(fn(x) -> x == 1 end)
+    |> Enum.map(fn x -> Enum.at(bits, x) end)
+    |> Enum.all?(fn x -> x == 1 end)
   end
 
   @doc """
@@ -54,8 +61,11 @@ defmodule BloomFilter do
   @spec add(t, any) :: t
   def add(%BloomFilter{bits: bits, hash_functions: hash_functions} = bloom, item) do
     item_bits = hash(hash_functions, item)
-    new_bits = item_bits
-      |> Enum.reduce(bits, fn(index, vector) -> List.replace_at(vector, index, 1) end)
+
+    new_bits =
+      item_bits
+      |> Enum.reduce(bits, fn index, vector -> List.replace_at(vector, index, 1) end)
+
     %{bloom | bits: new_bits}
   end
 
@@ -64,7 +74,7 @@ defmodule BloomFilter do
   """
   @spec count(t) :: float
   def count(%BloomFilter{bits: bits, num_bits: num_bits, hash_functions: hash_functions}) do
-    num_truthy_bits = Enum.count(bits, fn(x) -> x == 1 end)
+    num_truthy_bits = Enum.count(bits, fn x -> x == 1 end)
     approximate_size(num_bits, Enum.count(hash_functions), num_truthy_bits)
   end
 
@@ -73,27 +83,27 @@ defmodule BloomFilter do
   # https://en.wikipedia.org/wiki/Bloom_filter#Approximating_the_number_of_items_in_a_Bloom_filter
   @spec approximate_size(pos_integer, pos_integer, non_neg_integer) :: float
   defp approximate_size(m, k, x) do
-    -(m * (Math.log (1 - x/m), Math.e))/k
+    -(m * Math.log(1 - x / m, Math.e())) / k
   end
 
   # Calculates the false positive rate given m bits, n elements, and k hash functions
   # https://en.wikipedia.org/wiki/Bloom_filter#Probability_of_false_positives
   @spec false_positive_rate(pos_integer, pos_integer, number) :: float
   defp false_positive_rate(m, n, k) do
-    Math.pow (1 - Math.pow(Math.e, -k*(n+0.5)/(m-1))), k
+    Math.pow(1 - Math.pow(Math.e(), -k * (n + 0.5) / (m - 1)), k)
   end
 
   # Calculates the optimal number of hash functions k given m bits and capacity n
   # https://en.wikipedia.org/wiki/Bloom_filter#Optimal_number_of_hash_functions
   @spec optimal_hash_functions(pos_integer, pos_integer) :: float
   defp optimal_hash_functions(m, n) do
-    (m/n) * Math.log Math.e, 2
+    m / n * Math.log(Math.e(), 2)
   end
 
   # Calculates optimal bloom filter size m and number of hash functions k
   @spec optimize(pos_integer, float) :: {pos_integer, pos_integer, float}
   defp optimize(n, error_rate) do
-    optimize_values(n, n*4, 2, error_rate)
+    optimize_values(n, n * 4, 2, error_rate)
   end
 
   # Recursively calculates the optimal bloom filter parameters until the desired
@@ -102,18 +112,20 @@ defmodule BloomFilter do
   defp optimize_values(n, m, k, required_error_rate) do
     error_rate = false_positive_rate(m, n, k)
     acceptable_error_rate? = error_rate < required_error_rate
+
     cond do
       acceptable_error_rate? ->
-        {m, (k |> Float.ceil |> round), error_rate}
+        {m, k |> Float.ceil() |> round, error_rate}
+
       true ->
-        optimize_values(n, m*2, optimal_hash_functions(m*2, n), error_rate)
+        optimize_values(n, m * 2, optimal_hash_functions(m * 2, n), error_rate)
     end
   end
 
   # Generates k hash functions
   @spec make_hashes(pos_integer, pos_integer) :: [hash_func, ...]
   defp make_hashes(m, k) do
-    Enum.map(1..k, fn(i) -> make_hash_i(i, m) end)
+    Enum.map(1..k, fn i -> make_hash_i(i, m) end)
   end
 
   # Generates a new i-th hash function for a filter of size m bits
@@ -121,12 +133,17 @@ defmodule BloomFilter do
   # https://en.wikipedia.org/wiki/Fowler-Noll-Vo_hash_function
   @spec make_hash_i(pos_integer, pos_integer) :: hash_func
   defp make_hash_i(i, m) do
-    fn(item) -> rem (:erlang.phash2(item, Math.pow(2,32)) + i * FNV.FNV1a.hash(Kernel.inspect(item), 128)), m end
+    fn item ->
+      rem(
+        :erlang.phash2(item, Math.pow(2, 32)) + i * FNV.FNV1a.hash(Kernel.inspect(item), 128),
+        m
+      )
+    end
   end
 
   # Returns a list of the result of applying every function in hash_functions to item
   @spec hash([hash_func, ...], any) :: [pos_integer, ...]
   defp hash(hash_functions, item) do
-    Enum.map(hash_functions, fn(h) -> h.(item) end)
+    Enum.map(hash_functions, fn h -> h.(item) end)
   end
 end
--- test/bloom_filter_test.exs
+++ test/bloom_filter_test.exs
@@ -3,12 +3,12 @@ defmodule BloomFilterTest do
   doctest BloomFilter
 
   test "has? for a nonexistent element should return false" do
-    f = BloomFilter.new 100, 0.001
+    f = BloomFilter.new(100, 0.001)
     assert BloomFilter.has?(f, 42) == false
   end
 
   test "basic adding and membership scenario" do
-    f = BloomFilter.new 100, 0.001
+    f = BloomFilter.new(100, 0.001)
     f = BloomFilter.add(f, 42)
     assert BloomFilter.has?(f, 42) == true
     assert BloomFilter.has?(f, "missing") == false
